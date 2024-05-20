c08e5a7730ccd4e244b531a11967df200cd7d6b8
JackCollett <jack.collett@live.co.uk>
2024-05-20 10:00:46 +0100

    all

--- app.py
+++ app.py
@@ -16,6 +16,46 @@ app = Flask(__name__)
 def get_emoji():
     return ":)"
 
+@app.route('/goodbye', methods=['POST'])
+def goodbye():
+    name = request.form['name']
+    return f"Goodbye {name}!"
+
+@app.route('/submit', methods=['POST'])
+def two_parameters():
+    name = request.form['name']
+    message = request.form['message']
+    return f"Thanks {name}, you sent this message: '{message}'"
+
+@app.route('/wave', methods=['GET'])
+def wave():
+    name = request.args['name']
+    return f"I'm waving at {name}"
+
+@app.route('/count_vowels', methods=['POST'])
+def count_vowels():
+    text = request.form['text']
+    vowels = 'aeiou'
+    count = 0
+    for letter in text:
+        if letter in vowels:
+            count += 1
+    return f'There are {count} vowels in "{text}"'
+
+@app.route('/sorted-names', methods=['POST'])
+def post_sorted_names():
+    if 'names' not in request.form:
+        return "You didn't submit any names!", 400
+    names = request.form['names']
+    individuals = sorted(names.split(","))
+    return ",".join(individuals)
+
+@app.route('/names', methods=['POST'])
+def post_new_name():
+    names = ['Julia', 'Alice', 'Karim']
+    names.append(request.form['names'])
+    return ", ".join(sorted(names))
+
 # This imports some more example routes for you to see how they work
 # You can delete these lines if you don't need them.
 from example_routes import apply_example_routes
--- tests/test_app.py
+++ tests/test_app.py
@@ -11,3 +11,81 @@ def test_get_emoji(web_client):
     assert response.data.decode("utf-8") == ":)"
 
 # === End Example Code ===
+
+# File: tests/test_app.py
+
+"""
+When: I make a POST request to /count_vowels
+And: I send "eee" as the body parameter text
+Then: I should get a 200 response with 3 in the message
+"""
+def test_post_count_vowels_eee(web_client):
+    response = web_client.post('/count_vowels', data={'text': 'eee'})
+    assert response.status_code == 200
+    assert response.data.decode('utf-8') == 'There are 3 vowels in "eee"'
+
+"""
+When: I make a POST request to /count_vowels
+And: I send "eunoia" as the body parameter text
+Then: I should get a 200 response with 5 in the message
+"""
+def test_post_count_vowels_eunoia(web_client):
+    response = web_client.post('/count_vowels', data={'text': 'eunoia'})
+    assert response.status_code == 200
+    assert response.data.decode('utf-8') == 'There are 5 vowels in "eunoia"'
+
+"""
+When: I make a POST request to /count_vowels
+And: I send "mercurial" as the body parameter text
+Then: I should get a 200 response with 4 in the message
+"""
+def test_post_count_vowels_mercurial(web_client):
+    response = web_client.post('/count_vowels', data={'text': 'mercurial'})
+    assert response.status_code == 200
+    assert response.data.decode('utf-8') == 'There are 4 vowels in "mercurial"'
+
+# POST /sort-names
+# names=Joe,Alice,Zoe,Julia,Kieran
+#  Expected response (200 OK):
+
+"""
+#  Alice,Joe,Julia,Kieran,Zoe
+"""
+def test_post_sorted_names(web_client):
+    response = web_client.post('/sorted-names', data={'names': 'Joe,Alice,Zoe,Julia,Kieran'})
+    assert response.status_code == 200
+    assert response.data.decode('utf-8') == 'Alice,Joe,Julia,Kieran,Zoe'
+
+# POST /sort-names
+# names=Alice,Julia,Zoe,Joe,Kieran
+#  Expected response (200 OK):
+"""
+#  Alice,Joe,Julia,Kieran,Zoe
+"""
+def test_post_partially_sorted_names(web_client):
+    response = web_client.post('/sorted-names', data={'names': 'Alice,Julia,Zoe,Kieran'})
+    assert response.status_code == 200
+    assert response.data.decode('utf-8') == 'Alice,Julia,Kieran,Zoe'
+
+# POST /sort-names
+# names=
+#  Expected response Invalid response code:
+"""
+#  You didn't submit any names!
+"""
+def test_post_no_names(web_client):
+    response = web_client.post('/sorted-names')
+    assert response.status_code == 400
+    assert response.data.decode('utf-8') == "You didn't submit any names!"
+
+# GET /names?add=Eddie
+#  Parameters:
+#    name: Eddie
+#  Expected response (200 OK):
+"""
+Julia, Alice, Karim, Eddie
+"""
+def test_add_eddie(web_client):
+    response = web_client.post('/names', data={'names': 'Eddie, Farim'})
+    assert response.status_code == 200
+    assert response.data.decode('utf-8') == 'Alice, Eddie, Farim, Julia, Karim'
